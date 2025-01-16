401e0ff3f1a30bf5297607f95893693d8c3001ae
BBAMO <gao0708@naver.com>
2025-01-16 15:50:11 +0900

    커피 If

---
+++ Assets/Scripts/If.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class If : MonoBehaviour
+{
+    float coffeeTemp = 85.0f;
+    float hotLimitTemp = 70.0f;
+    float coldLimitTemp = 40.0f;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        TemperatureTest();
+
+        coffeeTemp -= Time.deltaTime * 5f;
+    }
+
+    void TemperatureTest()
+    {
+        if(coffeeTemp > hotLimitTemp)
+        {
+            print("Coffee is too hot.");
+        }
+
+        else if(coffeeTemp < coldLimitTemp)
+        {
+            print("Coffee is too cold");
+        }
+
+        else
+        {
+            print("Coffee is just right.");
+        }
+    }
+}
