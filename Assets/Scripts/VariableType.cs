5996e7e68249d63424084147994b650d1078631a
BBAMO <gao0708@naver.com>
2025-01-16 13:48:29 +0900

    유니티 스크립트 0116

---
+++ Assets/Scripts/VariableType.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableType : MonoBehaviour
+{
+    // 필드
+    static public bool staticVariable = true; //정적 변수
+    
+    private int m_GlovalVariable = 1; //전역 변수
+
+    public int abCde = 5; // 유니티 에디터에서 보여진다
+
+    public string STR = "hello";
+
+
+    void Start()
+    {
+        int localVariable = 1; //지역 변수
+        int localVariable2;
+        //Debug.Log(localVariable2.ToString());
+        LocalVariable(1/*매개변수*/);
+        Debug.Log("abcde:" + abCde);
+        Debug.Log(STR);
+    }
+
+    // Update is called once per frame
+    void LocalVariable(int parameter/*매개 변수*/)
+    {
+        float localVariable  = parameter; //지역 변수
+    }
+}
