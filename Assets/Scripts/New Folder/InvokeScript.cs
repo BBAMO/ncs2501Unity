315fd3fd836e20d7036468a6fdb1f201cdd0464e
BBAMO <gao0708@naver.com>
2025-01-31 15:32:32 +0900

    Invoke

---
+++ Assets/Scripts/New Folder/InvokeScript.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeScript : MonoBehaviour
+{
+    enum State {make, stop};
+    State state = State.make;
+    public GameObject target;
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 2, 1);
+        state = State.make;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (state == State.make)
+            {
+                CancelInvoke("SpawnObject");
+                state = State.stop;
+            }
+            else if (state == State.stop)
+            {
+                InvokeRepeating("SpawnObject", 1, 1);
+                state = State.make;
+            }
+        }
+    }
+
+    void SpawnObject()
+    {
+        int a = UnityEngine.Random.Range(1, 10);
+        float x = UnityEngine.Random.Range(-2.0f, 2.0f);
+        float z = UnityEngine.Random.Range(-2.0f, 2.0f);
+        Instantiate(target, new Vector3(x, 3, z), Quaternion.identity);
+    }
+}
